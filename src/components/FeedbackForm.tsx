79bc3425bfad86e308e6b4e04e5869660c0fc743
Ade Farquhar <adefarquhar@Ades-MacBook-Air.local>
2024-03-15 18:12:21 -0700

    - handle text input change on form - make characters left display dynamic

--- src/components/FeedbackForm.tsx
+++ src/components/FeedbackForm.tsx
@@ -1,13 +1,30 @@
+import { useState } from "react";
+import { MAX_CHARACTERS } from "./lib/constants";
+
 export function FeedbackForm() {
+  const [text, setText] = useState('');
+
+  const handleOnChange = (e) => {
+    setText(e.target.value);
+  }
+
+  const charactersLeft = MAX_CHARACTERS - text.length;
+
   return (
     <form className="form">
-      <textarea id="feedback-textarea" placeholder="" />
+      <textarea
+        id="feedback-textarea"
+        onChange={handleOnChange}
+        placeholder=""
+        spellCheck={false}
+        value={text}
+      />
       <label htmlFor="feedback-textarea">
           Enter your feedback here, remember to #hashtag the company
       </label>
 
       <div>
-        <p className="u-italic">150</p>
+        <p className="u-italic">{charactersLeft}</p>
         <button>
           <span>Submit</span>
         </button>
